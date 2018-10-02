cd5ee5637a2a1b97d0f34e4fabdaf0000c689dfd
Tobias Heuer <kittobi1992@users.noreply.github.com>
2018-10-02 19:29:44 +0200

    Update Hypergraph.h

--- Hypergraph/Hypergraph.h
+++ Hypergraph/Hypergraph.h
@@ -22,6 +22,10 @@ class Hypergraph
         HEdge(int id, std::vector<int> nodeIds, int weight);
     };
 
+    // comment: I try to prevent the usage of std::map as often as I can because they are very slow in practice.
+    // However, I think for your use case it is okay, because you only need the structure for modelling the hypergraph
+    // and converting it to hMEtis-File-Format (no performance critical operations). Also you can use the partition id 
+    // of a database table as node id ( advantage of 1-to-1 mapping between partition id and node ids )
     std::map<int, HNode> nodes;
     std::map<int, HEdge> edges;
 
